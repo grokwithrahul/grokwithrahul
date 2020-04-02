dcd12578bcceccd0a8d7e4c59276bc1b9a380bf4
Rahul Prabhu <rahul.r.prabhu@gmail.com>
2020-04-02 16:08:41 +0530

    Added Class4

---
+++ python.start(learn)/class4_(notdone)/program.py
@@ -0,0 +1,9 @@
+"""
+Today, we just went over the previous code and clarified doubts.
+
+For our chatbot, we need to make sure it can:
+
+1. Tell jokes to the user
+2. Book planes/trains
+3. Manage contacts (Bonus)
+"""
